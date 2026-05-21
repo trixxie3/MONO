693837bdeaa64eb5d47e58be47b69ff1d307f999
Ксения Стигнеева <kseniastigneeva@gmail.com>
2026-05-21 21:23:16 +0700

    update burger menu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
 import './_BurgerMenu.scss';
 import { Link } from 'react-router-dom';
 import Socials from '../Socials/Socials';
 
 export default function BurgerMenu({ isOpen, setIsBurgerOpen }) {
+  const [openAccordion, setOpenAccordion] = useState(null);
+  const closeMenu = () => setIsBurgerOpen(false);
+
+  const BurgerLink = ({ to, className, children }) => (
+    <Link className={className} to={to} onClick={closeMenu}>
+      {children}
+    </Link>
+  );
+
   return (
     <>
       <aside className={`burger-menu ${isOpen ? 'burger-menu--open' : ''}`}>
@@ -15,181 +25,150 @@ export default function BurgerMenu({ isOpen, setIsBurgerOpen }) {
           <span className="burger-menu__close-line"></span>
           <span className="burger-menu__close-line"></span>
         </button>
+
         <nav className="burger-menu__catalog">
           <h2 className="burger-menu__title burger-menu__title--catalog">
             Каталог
           </h2>
+
           <ul className="burger-menu__list">
-            <li className="burger-menu__item burger-menu__item--category">
-              <details className="accordion">
-                <summary className="accordion__title-wrap">
-                  <Link className="accordion__title" to="/category">
-                    Категория 1
-                  </Link>
-                  <svg
-                    className="accordion__title-icon"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 12 24"
+            {[1, 2, 3].map((item, index) => (
+              <li
+                key={item}
+                className="burger-menu__item burger-menu__item--category"
+              >
+                <div
+                  className={`accordion ${
+                    openAccordion === index ? 'accordion--open' : ''
+                  }`}
+                >
+                  <button
+                    type="button"
+                    className="accordion__title-wrap"
+                    onClick={() =>
+                      setOpenAccordion(openAccordion === index ? null : index)
+                    }
                   >
-                    <path
-                      fill="#1c2738"
-                      fill-rule="evenodd"
-                      d="M10.157 12.711L4.5 18.368l-1.414-1.414l4.95-4.95l-4.95-4.95L4.5 5.64l5.657 5.657a1 1 0 0 1 0 1.414"
-                    />
-                  </svg>
-                </summary>
-                <ul className="accordion__list">
-                  <li className="accordion__item-wrap">
-                    <Link className="accordion__item" to="/subcategory">
-                      Подкатегория 1
-                    </Link>
-                  </li>
-                  <li className="accordion__item-wrap">
-                    <Link className="accordion__item" to="/subcategory">
-                      Подкатегория 2
-                    </Link>
-                  </li>
-                  <li className="accordion__item-wrap">
-                    <Link className="accordion__item" to="/subcategory">
-                      Подкатегория 3
-                    </Link>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li className="burger-menu__item burger-menu__item--category">
-              <details className="accordion">
-                <summary className="accordion__title-wrap">
-                  <Link className="accordion__title" to="/category">
-                    Категория 1
-                  </Link>
-                  <svg
-                    className="accordion__title-icon"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 12 24"
-                  >
-                    <path
-                      fill="#1c2738"
-                      fill-rule="evenodd"
-                      d="M10.157 12.711L4.5 18.368l-1.414-1.414l4.95-4.95l-4.95-4.95L4.5 5.64l5.657 5.657a1 1 0 0 1 0 1.414"
-                    />
-                  </svg>
-                </summary>
-                <ul className="accordion__list">
-                  <li className="accordion__item-wrap">
-                    <Link className="accordion__item" to="/subcategory">
-                      Подкатегория 1
-                    </Link>
-                  </li>
-                  <li className="accordion__item-wrap">
-                    <Link className="accordion__item" to="/subcategory">
-                      Подкатегория 2
-                    </Link>
-                  </li>
-                  <li className="accordion__item-wrap">
-                    <Link className="accordion__item" to="/subcategory">
-                      Подкатегория 3
-                    </Link>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li className="burger-menu__item burger-menu__item--category">
-              <details className="accordion">
-                <summary className="accordion__title-wrap">
-                  <Link className="accordion__title" to="/category">
-                    Категория 1
-                  </Link>
-                  <svg
-                    className="accordion__title-icon"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 12 24"
-                  >
-                    <path
-                      fill="#1c2738"
-                      fill-rule="evenodd"
-                      d="M10.157 12.711L4.5 18.368l-1.414-1.414l4.95-4.95l-4.95-4.95L4.5 5.64l5.657 5.657a1 1 0 0 1 0 1.414"
-                    />
-                  </svg>
-                </summary>
-                <ul className="accordion__list">
-                  <li className="accordion__item-wrap">
-                    <Link className="accordion__item" to="/subcategory">
-                      Подкатегория 1
-                    </Link>
-                  </li>
-                  <li className="accordion__item-wrap">
-                    <Link className="accordion__item" to="/subcategory">
-                      Подкатегория 2
-                    </Link>
-                  </li>
-                  <li className="accordion__item-wrap">
-                    <Link className="accordion__item" to="/subcategory">
-                      Подкатегория 3
-                    </Link>
-                  </li>
-                </ul>
-              </details>
-            </li>
+                    <BurgerLink className="accordion__title" to="/category">
+                      Категория {item}
+                    </BurgerLink>
+
+                    <svg
+                      className="accordion__title-icon"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 12 24"
+                    >
+                      <path
+                        fill="#1c2738"
+                        fillRule="evenodd"
+                        d="M10.157 12.711L4.5 18.368l-1.414-1.414l4.95-4.95l-4.95-4.95L4.5 5.64l5.657 5.657a1 1 0 0 1 0 1.414"
+                      />
+                    </svg>
+                  </button>
+
+                  <div className="accordion__content">
+                    <div className="accordion__content-inner">
+                      <ul className="accordion__list">
+                        <li className="accordion__item-wrap">
+                          <BurgerLink
+                            className="accordion__item"
+                            to="/subcategory"
+                          >
+                            Подкатегория 1
+                          </BurgerLink>
+                        </li>
+
+                        <li className="accordion__item-wrap">
+                          <BurgerLink
+                            className="accordion__item"
+                            to="/subcategory"
+                          >
+                            Подкатегория 2
+                          </BurgerLink>
+                        </li>
+
+                        <li className="accordion__item-wrap">
+                          <BurgerLink
+                            className="accordion__item"
+                            to="/subcategory"
+                          >
+                            Подкатегория 3
+                          </BurgerLink>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
           </ul>
         </nav>
+
         <nav className="burger-menu__main">
           <h2 className="burger-menu__title">Основное</h2>
+
           <ul className="burger-menu__list burger-menu__list--main">
             <li className="burger-menu__item">
-              <Link className="burger-menu__link" to="/catalog">
+              <BurgerLink className="burger-menu__link" to="/catalog">
                 Каталог
-              </Link>
+              </BurgerLink>
             </li>
+
             <li className="burger-menu__item">
-              <Link className="burger-menu__link" to="/about">
+              <BurgerLink className="burger-menu__link" to="/about">
                 О компании
-              </Link>
+              </BurgerLink>
             </li>
+
             <li className="burger-menu__item">
-              <Link className="burger-menu__link" to="/contacts">
+              <BurgerLink className="burger-menu__link" to="/contacts">
                 Контакты
-              </Link>
+              </BurgerLink>
             </li>
+
             <li className="burger-menu__item">
-              <Link className="burger-menu__link" to="/delivery">
+              <BurgerLink className="burger-menu__link" to="/delivery">
                 Доставка
-              </Link>
+              </BurgerLink>
             </li>
+
             <li className="burger-menu__item">
-              <Link className="burger-menu__link" to="/payment">
+              <BurgerLink className="burger-menu__link" to="/payment">
                 Оплата
-              </Link>
+              </BurgerLink>
             </li>
+
             <li className="burger-menu__item">
-              <Link className="burger-menu__link" to="/profile">
+              <BurgerLink className="burger-menu__link" to="/account">
                 Личный кабинет
-              </Link>
+              </BurgerLink>
             </li>
+
             <li className="burger-menu__item">
-              <Link className="burger-menu__link" to="/blog">
+              <BurgerLink className="burger-menu__link" to="/blog">
                 Блог
-              </Link>
+              </BurgerLink>
             </li>
           </ul>
         </nav>
+
         <div className="burger-menu__contacts">
           <h2 className="burger-menu__title">Контакты</h2>
+
           <a href="tel:+7999999999" className="burger-menu__phone">
             +7 (999) 999-99-99
           </a>
+
           <p className="burger-menu__address">
             г. Москва, ул. Новорязанская, 18, стр. 11
           </p>
         </div>
+
         <Socials />
       </aside>
+
       <div
         className={`burger-menu-overlay ${
           isOpen ? 'burger-menu-overlay--open' : ''
